e2c4a7d48121592566b0aee8bdcbd26b316e59e3
DEYO <mdeyo@mit.edu>
2016-11-02 22:30:57 -0400

    started canvas work with icons

--- trek.js
+++ trek.js
@@ -13,6 +13,42 @@ function init() {
     warp_select = document.getElementById("warp_speed_select");
     course_select = document.getElementById("course_select");
 
+    myCanvas();
+
+}
+
+
+
+function myCanvas() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var width = 50;
+    var height = 50;
+    // ctx.fillRect(100, 10, 150, 80);
+
+    var starImg = new Image();
+    starImg.src = "images/power.png";
+    var starbaseImg = new Image();
+    starbaseImg.src = "images/starbase1.png";
+    var klingonImg = new Image();
+    klingonImg.src = "images/klingon.png";
+    var img = new Image();
+    img.src = "images/enterprise1.png";
+
+    starImg.onload = function() {
+        ctx.drawImage(starImg, 90, 10, width, height);
+        ctx.drawImage(starImg, 20, 70, width, height);
+    }
+    starbaseImg.onload = function() {
+        ctx.drawImage(starbaseImg, 200, 40, width - 10, height - 10);
+    }
+    klingonImg.onload = function() {
+        ctx.drawImage(klingonImg, 140, 100, width, height);
+    }
+    img.onload = function() {
+        ctx.drawImage(img, 10, 10, width, height);
+    }
+
 }
 
 function startGame() {
