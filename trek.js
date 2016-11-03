8a8484d18f97edb3e2f206f4f88ebebac43707b5
Matt Deyo <mdeyo@mit.edu>
2016-11-03 11:53:20 -0400

    updating with work done on lab computer

--- trek.js
+++ trek.js
@@ -3,6 +3,7 @@ var coverup;
 var panels;
 var warp_select;
 var course_select;
+var messageText;
 
 function init() {
     navigatePanel = document.getElementById("navigate_panel");
@@ -12,38 +13,78 @@ function init() {
 
     warp_select = document.getElementById("warp_speed_select");
     course_select = document.getElementById("course_select");
+    messageText = document.getElementById("message_text");
+}
 
+var post_result;
+
+function post(address, obj, success_function) {
+    var post_result;
+    $.ajax({
+        type: "POST",
+        method: "POST",
+        crossDomain: true,
+        url: address,
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify(obj),
+        success: success_function
+    });
 }
 
 function startGame() {
     console.log("startGame");
+    post("http://localhost:8080/start", {}, function(result) {
+        console.log("startGame result:");
+        try {
+            post_result = JSON.parse(result);
+        } catch (err) {
+            if (err.name == 'SyntaxError') {
+                // just a string return from server
+                post_result = result
+            } else {
+                post_result = " ";
+            }
+        }
+        console.log(post_result);
+        messageText.innerHTML = post_result.message;
+    });
+}
 
+
+function toggleNavigate() {
+    navigatePanel.style.display = "block";
+    coverup.style.display = "block";
 }
 
 function hidePanels() {
-    console.log("hidePanels");
+    //console.log("hidePanels");
     coverup.style.display = "none";
     for (i in panels) {
         panels[i].style.display = "none";
     }
 }
 
-function toggleNavigate() {
-    console.log(navigatePanel);
-    // if (navigatePanel.style.display == "none") {
-    navigatePanel.style.display = "block";
-    coverup.style.display = "block";
-    // } else {
-    // navigatePanel.style.display = "none";
-    // coverup.style.display = "none";
-    // }
-}
+var course_selected;
+var speed_selected;
 
-function sendNavigation() {
-    var course_selected = course_select.value;
-    var speed_selected = warp_select.value;
-    console.log("course " + course_selected);
-    console.log("speed " + speed_selected);
-    hidePanels();
+function navigate() {
+    course_selected = course_select.value;
+    speed_selected = warp_select.value;
+    console.log("navigate - course:" + course_selected + " and speed:" + speed_selected);
+    post("http://localhost:8080/nav", {
+        "course": parseInt(course_selected),
+        "speed": parseInt(speed_selected)
+    }, function(result) {
+        hidePanels();
+        console.log("navigate result:");
+        if (typeof result == 'string') {
+            post_result = JSON.parse(result);
+        } else {
+            post_result = result;
+        }
+        messageText.innerHTML = post_result.message;
 
+        console.log(post_result);
+    });
 }
